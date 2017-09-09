0bd3e8d1240f5441040ff55d3f82511c81367585
Maxime LEGENDRE <mlegendr@e2r9p19.42.fr>
2017-09-09 16:47:12 +0200

    Read file

--- n_puzzle/src/main.rs
+++ n_puzzle/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+fn read_file(filename: &String) {
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    match args.len() {
+        2 => read_file(&args[1]),
+        _ => println!("Hello, world!"),
+    }
 }
