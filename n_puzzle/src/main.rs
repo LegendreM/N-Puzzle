6bfcd3926feeeb958edd9517c739b1bbc2920859
wormsi <remidc.27@hotmail.fr>
2017-09-10 19:45:17 +0200

    Parse comments in file

--- n_puzzle/src/main.rs
+++ n_puzzle/src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::str::Lines;
 
-fn read_file(filename: &String) {
+fn read_file(filename: &String) -> String {
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
@@ -10,13 +11,40 @@ fn read_file(filename: &String) {
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+    contents
+}
+
+fn remove_comments(lines: Lines) -> Vec<&str> {
+    let mut ret: Vec<&str> = Vec::new();
+
+    for line in lines {
+        match line.chars().nth(0) {
+            Some('#') => continue,
+            Some(_) => {
+                match line.split("#").next() {
+                    Some(s) => ret.push(s),
+                    None => continue,
+                }
+            },
+            _ => continue,
+        }
+    }
+    ret
+}
+
+fn parse_file(content: &String) {
+    let lines = content.lines();
+    println!("Without comments:\n{:?}", remove_comments(lines));
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     match args.len() {
-        2 => read_file(&args[1]),
-        _ => println!("Hello, world!"),
+        2 => {
+            let contents = read_file(&args[1]);
+            parse_file(&contents);
+        },
+        _ => println!("No args :("),
     }
 }
