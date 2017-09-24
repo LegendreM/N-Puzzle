929cbf63dba0bb9b7a4584385317040e7b3afb4b
Maxime LEGENDRE <mlegendr@e2r7p22.42.fr>
2017-09-24 12:51:53 +0200

    Parse files

--- n_puzzle/src/main.rs
+++ n_puzzle/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::str::Lines;
+use std::process;
 
 fn read_file(filename: &String) -> String {
     let mut f = File::open(filename).expect("file not found");
@@ -32,19 +33,82 @@ fn remove_comments(lines: Lines) -> Vec<&str> {
     ret
 }
 
-fn parse_file(content: &String) {
+fn is_teals_valid(board: &Vec<Vec<u32>>, line_count: usize) -> bool {
+    let my_vec = board.clone();
+
+    let mut flatted: Vec<_> = my_vec.iter().flat_map(|v| v.iter()).collect();
+    let before_dedup_len = flatted.len();
+
+    flatted.sort();
+    flatted.dedup();
+
+    if before_dedup_len == flatted.len() {
+        if flatted.iter().any(|v| v >= &&(before_dedup_len as u32)) {
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn parse_file(content: &String) -> Result<Vec<Vec<u32>>, String> {
     let lines = content.lines();
-    println!("Without comments:\n{:?}", remove_comments(lines));
+    let str_board = remove_comments(lines);
+    println!("Without comments:\n{:?}", str_board);
+    let line_count;
+    match str_board[0].parse::<usize>() {
+        Ok(n) => line_count = n,
+        Err(e) => return Err(format!("error parsing line count: {:?}", e)),
+    }
+    println!("line count: {:?}", line_count);
+    if str_board.len() != line_count + 1 {
+        return Err(format!("error parsing line count: line count != {:?}", line_count));
+    }
+    let mut board: Vec<Vec<u32>> = Vec::new(); 
+    for line in str_board.iter().skip(1) {
+        let board_line: Result<Vec<_>, _> = line.split_whitespace()
+                .map(|s| s.parse())
+                .collect();
+        match board_line {
+            Ok(v) => {
+                    if v.len() == line_count {
+                        board.push(v);
+                    }
+                    else {
+                        return Err(format!("error parsing line count: col count != {:?}", line_count))
+                    }
+                },
+            Err(e) => return Err(format!("error parsing line count: {}", e)),
+        }
+    }
+    println!("board: {:?}", board);
+    if is_teals_valid(&board, line_count) {
+        return Ok(board);
+    }
+    return Err(format!("error parsing teals"));
+}
+fn failable_main() -> Result<(), Box<::std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        let contents = read_file(&args[1]);
+        if contents.is_empty() {
+            return Err(format!("Empty file").into());
+        }
+        else {
+            let board = parse_file(&contents)?;
+        }
+    }
+    else {
+        return Err(format!("No args :(").into());
+    }
+    Ok(())
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    match args.len() {
-        2 => {
-            let contents = read_file(&args[1]);
-            parse_file(&contents);
-        },
-        _ => println!("No args :("),
+    if let Err(e) = failable_main() {
+        eprintln!("{}", e);
+        process::exit(1);
     }
 }
