4ca328302f968da2bb82ebb0082d7675effd3fa6
Maxime LEGENDRE <mlegendr@e1r12p18.42.fr>
2017-12-22 14:45:35 +0100

    Add Clap

--- n_puzzle/src/main.rs
+++ n_puzzle/src/main.rs
@@ -1,5 +1,7 @@
 extern crate n_puzzle;
+extern crate clap;
 
+use clap::{Arg, App, SubCommand};
 use std::env;
 use std::fs::File;
 use std::io::Read;
@@ -7,7 +9,7 @@ use std::str::Lines;
 use std::process;
 
 use n_puzzle::{Board, Solver, Tile};
-use n_puzzle::{Manhattan, Dijkstra, Euclidean};
+use n_puzzle::{Manhattan, Dijkstra, Euclidean, MissPlaced};
 
 fn read_file(filename: &String) -> String {
     let mut f = File::open(filename).expect("file not found");
@@ -90,35 +92,73 @@ fn parse_file(content: &String) -> Result<Vec<Vec<Tile>>, String> {
 fn failable_main() -> Result<(), Box<::std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 3 {
-        let contents = read_file(&args[1]);
-        if contents.is_empty() {
-            return Err(format!("Empty file").into());
-        }
-        let expected = read_file(&args[2]);
-        if expected.is_empty() {
-            return Err(format!("Empty file").into());
-        }
-        let board_vec: Vec<Vec<Tile>> = parse_file(&contents)?;
-        let line_size = board_vec.len();
-        let board_vec: Vec<Tile> = board_vec.into_iter().flat_map(|v| v.into_iter()).collect();
-        let board = Board::new(board_vec.into_boxed_slice(), line_size);
+    let matches = App::new("npuzzle")
+                          .version("1.0")
+                          .about("A* agorithm to solve npuzzle")
+                          .arg(Arg::with_name("INPUT")
+                               .short("i")
+                               .long("input")
+                               .value_name("FILE")
+                               .help("Input file which contains the npuzzle to solve")
+                               .required(true))
+                          .arg(Arg::with_name("EXPECTED")
+                               .short("e")
+                               .long("expected")
+                               .value_name("FILE")
+                               .help("Input file which contains the npuzzle expected solution")
+                               .required(true))
+                          .arg(Arg::with_name("HEURISTIC")
+                               .short("d")
+                               .long("distance")
+                               .value_name("STRING")
+                               .help("Heuristic used to solve npuzzle")
+                               .required(true))
+                          .get_matches();
 
-        let expected_vec: Vec<Vec<Tile>> = parse_file(&expected)?;
-        let expected_line_size = expected_vec.len();
-        let expected_vec: Vec<Tile> = expected_vec.into_iter().flat_map(|v| v.into_iter()).collect();
-        let expected = Board::new(expected_vec.into_boxed_slice(), line_size);
 
-        match Solver::new(board, expected) {
-            Ok(solver) => {
-                let (mem, time, moves) = solver.solve::<Manhattan>();
-                println!("memory complexity: {}\ntime complexity: {}\nmoves count: {}\nmoves:\n{:?}", mem, time, moves.len(), moves);
-            },
-            Err(e) => println!("{}", e)
-        }
+
+    let input = matches.value_of("INPUT").unwrap();
+    println!("Value for input: {}", input);
+
+    let expected = matches.value_of("EXPECTED").unwrap();
+    println!("Value for expected: {}", expected);
+
+    let heuristic = matches.value_of("HEURISTIC").unwrap();
+    println!("Value for heuristic: {}", heuristic);
+
+    let contents = read_file(&String::from(input));
+    if contents.is_empty() {
+        return Err(format!("Empty input file").into());
     }
-    else {
-        return Err(format!("No args :(").into());
+    let expected = read_file(&String::from(expected));
+    if expected.is_empty() {
+        return Err(format!("Empty expected file").into());
+    }
+    let board_vec: Vec<Vec<Tile>> = parse_file(&contents)?;
+    let line_size = board_vec.len();
+    let board_vec: Vec<Tile> = board_vec.into_iter().flat_map(|v| v.into_iter()).collect();
+    let board = Board::new(board_vec.into_boxed_slice(), line_size);
+
+    let expected_vec: Vec<Vec<Tile>> = parse_file(&expected)?;
+    let expected_line_size = expected_vec.len();
+    let expected_vec: Vec<Tile> = expected_vec.into_iter().flat_map(|v| v.into_iter()).collect();
+    let expected = Board::new(expected_vec.into_boxed_slice(), line_size);
+
+    match Solver::new(board, expected) {
+        Ok(solver) => {
+            let result;
+
+            match heuristic {
+                "manhattan" => result = solver.solve::<Manhattan>(),
+                "dijkstra" => result = solver.solve::<Dijkstra>(),
+                "euclidean" => result = solver.solve::<Euclidean>(),
+                "miss_placed" => result = solver.solve::<MissPlaced>(),
+                _ => result = solver.solve::<MissPlaced>(),
+            }
+            let (mem, time, moves) = result;
+            println!("memory complexity: {}\ntime complexity: {}\nmoves count: {}\nmoves:\n{:?}", mem, time, moves.len(), moves);
+        },
+        Err(e) => println!("{}", e)
     }
     Ok(())
 }
