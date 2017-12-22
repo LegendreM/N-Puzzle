89455ee0e5a575197e46805fd32dbb6f1223211d
Maxime LEGENDRE <mlegendr@e1r12p18.42.fr>
2017-12-22 11:25:07 +0100

    Add Euclidean distance

--- n_puzzle/src/heuristic/euclidean.rs
+++ n_puzzle/src/heuristic/euclidean.rs
@@ -11,14 +11,16 @@ impl Heuristic for Euclidean {
     }
 
     fn distance(&self, current: &Board) -> usize {
-        // let line_size = current.line_size;
-        // let mut cost = 0;
-        // for (i, &tile) in current.data.iter().enumerate() {
-        //     let expected_pos = expected.data.iter().position(|&x| x == tile).unwrap();
-        //     let linear_dist = (i as isize - expected_pos as isize).abs() as usize;
-        //     cost += (linear_dist % line_size) + (linear_dist / line_size);
-        // }
-        // cost
-        unimplemented!()
+        let line_size = current.line_size as isize;
+        let mut cost = 0;
+        for (i, &tile) in current.data.iter().enumerate() {
+            let i = i as isize;
+            let (exp_x, exp_y) = self.positions[tile as usize];
+            let (cur_x, cur_y) = (i % line_size, i / line_size);
+            let x_sum = exp_x - cur_x;
+            let y_sum = exp_y - cur_y;
+            cost += ((x_sum * x_sum + y_sum * y_sum) as f64).sqrt() as usize;
+        }
+        cost
     }
 }
