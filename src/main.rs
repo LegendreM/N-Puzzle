ded5ecb881605c26752b896a9342d98ad7e8ede8
Clément Renault <renault.cle@gmail.com>
2019-05-08 19:33:39 +0200

    Improve the map parsing

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,132 @@
 use std::fs::File;
-use std::io::Read;
-use std::path::{Path, PathBuf};
-use std::process;
-use std::str::Lines;
-use std::error::Error;
+use std::io::{self, Read, BufRead, BufReader};
+use std::num::ParseIntError;
+use std::path::PathBuf;
+use std::{fmt, process};
+use std::str::FromStr;
 
 use structopt::StructOpt;
 
 use n_puzzle::{Board, Solver, Tile};
 use n_puzzle::{Manhattan, Dijkstra, Euclidean, MissPlaced, OutOfRaw};
 
-fn read_file(filename: &Path) -> Result<String, String> {
-    let f = File::open(filename);
-    match f {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            match file.read_to_string(&mut contents) {
-                Ok(_) => Ok(contents),
-                Err(e) => Err(format!("error in read file: {:?}", e))
+#[derive(Debug)]
+enum Error {
+    PuzzleInvalidTiles,
+    PuzzleMissingSize,
+    PuzzleInvalidNumberOfTiles,
+    PuzzleInvalidNumber(ParseIntError),
+    IoError(io::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Error {
+        Error::IoError(error)
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(error: ParseIntError) -> Error {
+        Error::PuzzleInvalidNumber(error)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::PuzzleInvalidTiles => write!(f, "invalid tiles"),
+            Error::PuzzleMissingSize => write!(f, "missing puzzle size"),
+            Error::PuzzleInvalidNumberOfTiles => write!(f, "invalid number of tiles"),
+            Error::PuzzleInvalidNumber(error) => write!(f, "invalid tile number; {}", error),
+            Error::IoError(error) => write!(f, "io error; {}", error),
+        }
+    }
+}
+
+impl std::error::Error for Error { }
+
+fn is_board_valid(numbers: &[Tile]) -> bool {
+    let sort_dedup_numbers = {
+        let mut numbers = numbers.to_vec();
+        numbers.sort_unstable();
+        numbers.dedup();
+        numbers
+    };
+
+    if sort_dedup_numbers.len() != numbers.len() {
+        return false
+    }
+
+    if sort_dedup_numbers.get(0) != Some(&0) {
+        return false
+    }
+
+    for array in sort_dedup_numbers.windows(2) {
+        if array[1] != array[0] + 1 {
+            return false
+        }
+    }
+
+    true
+}
+
+fn no_comment(string: &str) -> Option<&str> {
+    string.split('#').next().filter(|s| !s.is_empty())
+}
+
+fn read_board<R: Read>(read: R) -> Result<(Box<[Tile]>, usize), Error> {
+    let read = BufReader::new(read);
+    let mut lines = read.lines();
+
+    // retrieve the puzzle size
+    let mut size = None;
+    for line in &mut lines {
+        let line = line?;
+        if let Some(number_part) = no_comment(&line) {
+            let trimmed = number_part.trim();
+            if !trimmed.is_empty() {
+                size = Some(usize::from_str(trimmed)?);
+                break;
             }
-        },
-        Err(e) => Err(format!("error in read file: {:?}", e))
-    }
-
-}
-
-fn remove_comments(lines: Lines) -> Vec<&str> {
-    let mut ret: Vec<&str> = Vec::new();
-
-    for line in lines {
-        match line.chars().nth(0) {
-            Some('#') => continue,
-            Some(_) => {
-                match line.split("#").next() {
-                    Some(s) => ret.push(s),
-                    None => continue,
-                }
-            },
-            _ => continue,
         }
     }
-    ret
-}
 
-fn is_teals_valid(board: &Vec<Vec<Tile>>) -> bool {
-    let my_vec = board.clone();
+    let size = match size {
+        Some(size) => size,
+        None => return Err(Error::PuzzleMissingSize),
+    };
 
-    let mut flatted: Vec<_> = my_vec.iter().flat_map(|v| v.iter()).collect();
-    let before_dedup_len = flatted.len();
+    // retrieve the tiles numbers
+    let mut numbers = Vec::with_capacity(size * size);
+    for line in &mut lines {
+        let line = line?;
+        if let Some(tiles_part) = no_comment(&line) {
+            let prev_len = numbers.len();
 
-    flatted.sort();
-    flatted.dedup();
+            for number_part in tiles_part.split_whitespace() {
+                let number = Tile::from_str(number_part)?;
+                numbers.push(number);
+            }
 
-    if before_dedup_len == flatted.len() {
-        return !flatted.iter().any(|v| v >= &&(before_dedup_len as Tile));
-    } else {
-        return false;
-    }
-}
-
-fn parse_file(content: &String) -> Result<Vec<Vec<Tile>>, String> {
-    let lines = content.lines();
-    let str_board = remove_comments(lines);
-    let line_count;
-
-    if str_board.len() == 0 {
-        return Err(format!("file doesn't contain board"))
-    }
-    match str_board[0].parse::<usize>() {
-        Ok(n) => line_count = n,
-        Err(e) => return Err(format!("error parsing line count: {:?}", e)),
-    }
-    if str_board.len() != line_count + 1 {
-        return Err(format!("error parsing line count: line count != {:?}", line_count));
-    }
-    let mut board: Vec<Vec<Tile>> = Vec::new();
-    for line in str_board.iter().skip(1) {
-        let board_line: Result<Vec<_>, _> = line.split_whitespace()
-                .map(|s| s.parse())
-                .collect();
-        match board_line {
-            Ok(v) => {
-                    if v.len() == line_count {
-                        board.push(v);
-                    }
-                    else {
-                        return Err(format!("error parsing line count: col count != {:?}", line_count))
-                    }
-                },
-            Err(e) => return Err(format!("error parsing line count: {}", e)),
+            if numbers.len() - prev_len != size {
+                return Err(Error::PuzzleInvalidNumberOfTiles)
+            }
         }
     }
-    if is_teals_valid(&board) {
-        return Ok(board);
+
+    if numbers.len() != size * size {
+        return Err(Error::PuzzleInvalidNumberOfTiles)
     }
-    return Err(format!("error parsing teals"));
+
+    if !is_board_valid(&numbers) {
+        return Err(Error::PuzzleInvalidTiles)
+    }
+
+    Ok((numbers.into_boxed_slice(), size))
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "n-puzzle", about = "A* algorithm to solve npuzzle")]
+#[structopt(name = "n-puzzle", about = "A* algorithm to solve npuzzles")]
 struct Opt {
     /// Input file which contains the npuzzle to solve
     #[structopt(parse(from_os_str))]
@@ -112,39 +140,28 @@ struct Opt {
     heuristic: String,
 }
 
-fn failable_main() -> Result<(), Box<Error>> {
+fn failable_main() -> Result<(), Box<std::error::Error>> {
     let opt = Opt::from_args();
 
-    let input = opt.input;
-    println!("Value for input: {:?}", input);
+    println!("Value for input: {:?}", opt.input);
+    println!("Value for expected: {:?}", opt.expected);
+    println!("Value for heuristic: {}", opt.heuristic);
 
-    let expected = opt.expected;
-    println!("Value for expected: {:?}", expected);
+    let input = {
+        let input_file = File::open(&opt.input)?;
+        let (input_numbers, input_size) = read_board(input_file)?;
+        Board::new(input_numbers, input_size)
+    };
 
-    let heuristic = opt.heuristic;
-    println!("Value for heuristic: {}", heuristic);
+    let expected = {
+        let expected_file = File::open(&opt.expected)?;
+        let (expected_numbers, expected_size) = read_board(expected_file)?;
+        Board::new(expected_numbers, expected_size)
+    };
 
-    let contents = read_file(&input)?;
-    if contents.is_empty() {
-        return Err(format!("Empty input file").into());
-    }
-    let expected = read_file(&expected)?;
-    if expected.is_empty() {
-        return Err(format!("Empty expected file").into());
-    }
-    let board_vec: Vec<Vec<Tile>> = parse_file(&contents)?;
-    let line_size = board_vec.len();
-    let board_vec: Vec<Tile> = board_vec.into_iter().flat_map(|v| v.into_iter()).collect();
-    let board = Board::new(board_vec.into_boxed_slice(), line_size);
-
-    let expected_vec: Vec<Vec<Tile>> = parse_file(&expected)?;
-    let expected_line_size = expected_vec.len();
-    let expected_vec: Vec<Tile> = expected_vec.into_iter().flat_map(|v| v.into_iter()).collect();
-    let expected = Board::new(expected_vec.into_boxed_slice(), expected_line_size);
-
-    match Solver::new(board, expected) {
+    match Solver::new(input, expected) {
         Ok(solver) => {
-            let result = match heuristic.as_str() {
+            let result = match opt.heuristic.as_str() {
                 "manhattan" => solver.solve::<Manhattan>(),
                 "dijkstra" => solver.solve::<Dijkstra>(),
                 "euclidean" => solver.solve::<Euclidean>(),
